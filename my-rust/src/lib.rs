b6bbf17d4c387127fa70e8c992ecc828b9ffda52
khadiev <khadiev.edem@gmail.com>
2022-07-05 23:14:01 +0300

    remove example rust code

--- my-rust/src/lib.rs
+++ my-rust/src/lib.rs
@@ -2,18 +2,3 @@
 pub extern "C" fn rust_lib_function() {
     println!("Hello world!");
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
