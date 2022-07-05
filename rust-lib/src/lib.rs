940da3e7c340f18ca36721841a32d3179afc3f8a
khadiev <khadiev.edem@gmail.com>
2022-07-05 17:34:24 +0300

    init rust library

---
+++ rust-lib/src/lib.rs
@@ -0,0 +1,18 @@
+#[no_mangle]
+pub extern "C" fn rust_lib_function() {
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
